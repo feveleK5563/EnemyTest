d445622729a7e0000cef62a0106dba71cfe2e0da
DESKTOP-HGO5QJ2\bunch <buncha102828@gmail.com>
2018-04-02 23:12:27 +0900

    敵パラメータテスト

---
+++ 敵パラメータテスト/MyGameMain.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "MyPG.h"
+
+extern  void  MyGameMain_Finalize( );
+extern  void  MyGameMain_Initalize( );
+extern  void  MyGameMain_UpDate( );
+extern  void  MyGameMain_Render2D( );
